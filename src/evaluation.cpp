510538f0de74e08c35b5a3f6442eac17b523c32c
OnofreMartorell <onofremartorelln@gmail.com>
2017-06-16 10:31:03 +0200

    Evaluation works

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -1,11 +1,17 @@
 
-
-
-void  of_errors(flow1, flow2, w, h, pd){
+#include <cmath>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <string>
+extern "C" {
+#include "iio.h"
+}
+void  of_errors(float *flow1, float *flow2, int w, int h, int pd){
 
     //[ni, nj, nChannels] = size(flow1);
-    image_ae = new float[w*h];
-    image_ee = new float [w*h];
+    auto image_ae = new float[w*h];
+    auto image_ee = new float [w*h];
     float ae = 0;
     float ee = 0;
     int nPix = 0;
@@ -13,43 +19,45 @@ void  of_errors(flow1, flow2, w, h, pd){
         for (int i = 1; i <= w; i++){
 
             //Exactly as in C code
+            int p = i*h + j;
+            float u_c = flow1[p];//(i,j,1);
+            float v_c = flow1[p + w*h];//(i,j,2);
 
-            u_c = flow1[];//(i,j,1);
-            v_c = flow1[];//(i,j,2);
+            float u_e = flow2[p];//(i,j,1);
+            float v_e = flow2[p + w*h];//(i,j,2);
 
-            u_e = flow2[];//(i,j,1);
-            v_e = flow2[];//(i,j,2);
+            if ((std::abs(u_c) < 10000) && (std::abs(v_c) < 10000)){
+                nPix++;
+                float esc_prod = u_c*u_e + v_c*v_e + 1.0;
 
-            if ((abs(u_c) < 10000) && (abs(v_c) < 10000)){
-                nPix = nPix + 1;
-                esc_prod = u_c*u_e + v_c*v_e + 1.0;
+                float term1 = (u_c * u_c + v_c * v_c + 1.0);
+                float term2 = (u_e * u_e + v_e * v_e + 1.0);
+                float term3 = std::sqrt(term1 * term2);
 
-                term1 = (u_c * u_c + v_c * v_c + 1.0);
-                term2 = (u_e * u_e + v_e * v_e + 1.0);
-                term3 = sqrt(term1 * term2);
-
-                tmp = esc_prod / term3;
+                float tmp = esc_prod / term3;
 
 
-                tmp = min(tmp, 1.0);
-                tmp = max(tmp, -1.0);
+                tmp = std::fmin(tmp, 1.0);
+                tmp = std::fmax(tmp, -1.0);
 
                 ae = ae + acos(tmp);
-                image_ae(i, j) = acos(tmp);
-                ee = ee + sqrt((u_c - u_e)^2 + (v_c - v_e)^2);
-                image_ee(i, j) = sqrt((u_c - u_e)^2 + (v_c - v_e)^2);
+                image_ae[p] = acos(tmp);
+                ee = ee + std::sqrt((u_c - u_e)*(u_c - u_e) + (v_c - v_e)*(v_c - v_e));
+                image_ee[p] = std::sqrt((u_c - u_e)*(u_c - u_e) + (v_c - v_e)*(v_c - v_e));
             }
         }
     }
     ae = ae/nPix;
+    std::cout << "Angular error: "<< ae << "\n";
     ee = ee/nPix;
+    std::cout << "Endpoint error: "<< ee << "\n";
 }
 
 
 int main(int argc, char* argv[]){
 
     std::vector<std::string> args(argv, argv + argc);
-    if (args.size() != 2) {
+    if (args.size() != 3) {
         fprintf(stderr, "usage %d :\n\t%s out.flo gt.flo", args.size(), args[0].c_str());
 
         return 1;
@@ -72,6 +80,7 @@ int main(int argc, char* argv[]){
     //Sparse Optical flow forward and backward
     float *optical_flow = iio_read_image_float_split(filename_of.c_str(), w, h, pd);
     float *ground_truth = iio_read_image_float_split(filename_gt.c_str(), w + 1, h + 1, pd + 1);
+    of_errors(optical_flow, ground_truth, w[0], h[0], pd[0]);
     return 0;
 
 }
